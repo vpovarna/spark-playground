3e8211e8fb02ec2d64b9eac646bced4847e5b371
Vlad Povarna <povarna@adobe.com>
2022-06-24 17:08:08 +0300

    RDDs example

---
+++ src/main/scala/com/examples/utils/package.scala
@@ -0,0 +1,10 @@
+package com.examples
+
+package object utils {
+  def using[A <: {def close(): Unit}, B](resource: A)(f: A => B): B =
+    try {
+      f(resource)
+    } finally {
+      resource.close()
+    }
+}
