78cc35f9ffebf3b3e515446c3b5e61be1092a7ca
Vlad Povarna <povarna@adobe.com>
2022-07-21 14:00:29 +0300

    Integrating with Akka actors

--- build.sbt
+++ build.sbt
@@ -6,9 +6,13 @@ scalaVersion := "2.12.10"
 
 val sparkVersion = "3.0.2"
 val vegasVersion = "0.3.11"
+val postgresVersion = "42.2.2"
+val cassandraConnectorVersion = "3.0.0"
 val log4jVersion = "2.4.1"
+val twitter4jVersion = "4.0.7"
 val kafkaVersion = "2.4.0"
-
+val akkaHttpVersion = "10.1.7"
+val akkaVersion = "2.5.24"
 
 resolvers ++= Seq(
   "bintray-spark-packages" at "https://dl.bintray.com/spark-packages/maven",
@@ -30,6 +34,20 @@ libraryDependencies ++= Seq(
   // kafka
   "org.apache.kafka" %% "kafka" % kafkaVersion,
   "org.apache.kafka" % "kafka-streams" % kafkaVersion,
+
+  // akka
+  "com.typesafe.akka" %% "akka-remote" % akkaVersion,
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+
+  "com.datastax.spark" %% "spark-cassandra-connector" % cassandraConnectorVersion,
+
+  // postgres
+  "org.postgresql" % "postgresql" % postgresVersion,
+
+  // twitter
+  "org.twitter4j" % "twitter4j-core" % twitter4jVersion,
+  "org.twitter4j" % "twitter4j-stream" % twitter4jVersion,
 
   "org.apache.logging.log4j" % "log4j-api" % log4jVersion,
   "org.apache.logging.log4j" % "log4j-core" % log4jVersion
---
+++ src/main/scala/com/examples/streaming/integrations/IntegratingAkka.scala
@@ -0,0 +1,87 @@
+package com.examples.streaming.integrations
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
+import akka.stream.scaladsl.{Sink, Source}
+import com.examples.streaming.strucured.Car
+import com.examples.streaming.util.Schema.carsSchema
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+object IntegratingAkka {
+  val spark: SparkSession = SparkSession.builder()
+    .appName("Integrating Akka")
+    .master("local[2]")
+    .getOrCreate()
+
+  val sc: SparkContext = spark.sparkContext
+
+  import spark.implicits._
+
+  def writeCarsToAkka(): Unit = {
+    val carsDF = spark.readStream
+      .schema(carsSchema)
+      .json("src/main/resources/data/cars")
+
+    val carsDS = carsDF.as[Car]
+
+    carsDS.writeStream
+      .foreachBatch { (batch: Dataset[Car], batchId: Long) =>
+        batch.foreachPartition { cars: Iterator[Car] =>
+          // this code is run by a single executor
+
+          val system: ActorSystem = ActorSystem(s"SourceSystem$batchId", ConfigFactory.load("akkaconfig/remoteActors"))
+          val entryPoint = system.actorSelection("akka://ReceiverSystem@localhost:2552/user/entrypoint")
+
+          // send all the data
+          cars.foreach(car => entryPoint ! car)
+        }
+      }
+      .start()
+      .awaitTermination()
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    writeCarsToAkka()
+  }
+}
+
+object ReceiverSystem {
+  implicit val actorSystem: ActorSystem = ActorSystem("ReceiverSystem", ConfigFactory.load("akkaconfig/remoteActors").getConfig("remoteSystem"))
+  implicit val actorMaterializer: ActorMaterializer = ActorMaterializer()
+
+  class Destination extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case m => log.info(m.toString)
+    }
+  }
+
+  class EntryPoint(destination: ActorRef) extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case m =>
+        log.info(s"Received $m")
+        destination ! m
+    }
+  }
+
+  object EntryPoint {
+    def pops(destination: ActorRef): Props = Props(new EntryPoint(destination))
+  }
+
+  def main(args: Array[String]): Unit = {
+    // using actor system
+    //    val destination = actorSystem.actorOf(Props[Destination], "destination")
+    //    val entryPoint = actorSystem.actorOf(EntryPoint.pops(destination), "entrypoint")
+
+    // using akka streams
+    val source = Source.actorRef[Car](bufferSize = 10, overflowStrategy = OverflowStrategy.dropHead)
+    val sink = Sink.foreach[Car](println)
+    val runnableGraph = source.to(sink)
+    val actorRef: ActorRef = runnableGraph.run()
+
+    val entryPoint = actorSystem.actorOf(EntryPoint.pops(actorRef), "entrypoint")
+
+  }
+}
